7fa41b250636fdb0283da16e5a4e4a4c4e72a1f0
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-23 16:35:19 +0500

    making website

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from "./card";
+import S_data from "./S_data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="heading_style">List of Top 5 Netflix Series in 2021</h1>
+      <Card imgsrc={S_data[0].imgsrc} title={S_data[0].title} SName={S_data[0].SName} link={S_data[0].link}/>
+      <Card imgsrc={S_data[1].imgsrc} title={S_data[1].title} SName={S_data[1].SName} link={S_data[1].link}/>
+      <Card imgsrc={S_data[2].imgsrc} title={S_data[2].title} SName={S_data[2].SName} link={S_data[2].link}/>
+      <Card imgsrc={S_data[3].imgsrc} title={S_data[3].title} SName={S_data[3].SName} link={S_data[3].link}/>
+      <Card imgsrc={S_data[4].imgsrc} title={S_data[4].title} SName={S_data[4].SName} link={S_data[4].link}/>
     </div>
   );
 }
